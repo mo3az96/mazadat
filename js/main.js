aa1387ed61cdc0064d6b6187ae7d4edfd913d9d0
mo3az96 <mo3az.abdellatif@gmail.com>
2025-11-29 11:45:49 +0200

    add: join form

--- js/main.js
+++ js/main.js
@@ -72,3 +72,131 @@ $(document).ready(function () {
     }
   });
 });
+
+/************************************ Forms ************************************/
+$(document).ready(function () {
+  /************************************ Password ************************************/
+  $(".password-toggle").on("click", function (e) {
+    e.preventDefault();
+    const input = $(this)
+      .closest(".password-content")
+      .find("input.form-control");
+    const isPassword = input.attr("type") === "password";
+    input.attr("type", isPassword ? "text" : "password");
+    $(this).toggleClass("active", isPassword);
+  });
+  /************************************ Country Code Input ************************************/
+  let input = $("input[type=tel][intlTelInput]");
+  if (input.length > 0) {
+    for (let i = 0; i < input.length; i++) {
+      intlTelInput(input[i], {
+        utilsScript: "/js/utils.js",
+        separateDialCode: true,
+        initialCountry: "sa",
+        preferredCountries: ["sa", "kw", "ae", "bh", "om", "qa"],
+      });
+    }
+  }
+
+  /************************************ OTP ************************************/
+  const inputs = $("#otp-input input");
+  if (inputs.length > 0) {
+    inputs.on("input", function () {
+      const index = inputs.index(this);
+
+      if (this.value.length == 1 && index + 1 < inputs.length) {
+        $(inputs[index + 1]).removeAttr("disabled");
+        inputs[index + 1].focus();
+      } else {
+        inputs.blur();
+        $(".submit-btn").removeAttr("disabled");
+      }
+
+      if (this.value.length > 1) {
+        if (isNaN(this.value)) {
+          this.value = "";
+          updateInput();
+          return;
+        }
+
+        const chars = this.value.split("");
+
+        $.each(chars, function (pos) {
+          if (pos + index >= inputs.length) return false;
+
+          let targetInput = inputs[pos + index];
+          targetInput.value = chars[pos];
+        });
+
+        let focusIndex = Math.min(inputs.length - 1, index + chars.length);
+        inputs[focusIndex].focus();
+      }
+      updateInput();
+    });
+
+    inputs.on("keydown", function (e) {
+      const index = inputs.index(this);
+
+      if (e.keyCode == 8 && this.value == "" && index != 0) {
+        for (let pos = index; pos < inputs.length - 1; pos++) {
+          inputs[pos].value = inputs[pos + 1].value;
+        }
+
+        inputs[index - 1].value = "";
+        inputs[index - 1].focus();
+        updateInput();
+        return;
+      }
+
+      if (e.keyCode == 46 && index != inputs.length - 1) {
+        for (let pos = index; pos < inputs.length - 1; pos++) {
+          inputs[pos].value = inputs[pos + 1].value;
+        }
+
+        inputs[inputs.length - 1].value = "";
+        this.select();
+        e.preventDefault();
+        updateInput();
+        return;
+      }
+
+      if (e.keyCode == 37) {
+        if (index > 0) {
+          e.preventDefault();
+          inputs[index - 1].focus();
+          inputs[index - 1].select();
+        }
+        return;
+      }
+
+      if (e.keyCode == 39) {
+        if (index + 1 < inputs.length) {
+          e.preventDefault();
+          inputs[index + 1].focus();
+          inputs[index + 1].select();
+        }
+        return;
+      }
+    });
+
+    function updateInput() {
+      let inputValue = inputs.toArray().reduce(function (otp, input) {
+        otp += input.value.length ? input.value : " ";
+        return otp;
+      }, "");
+      $("input[name=otp]").val(inputValue);
+    }
+  }
+  /************************************ Upload File ************************************/
+  $(document).on("change", ".file-input", function () {
+    const box = $(this).closest(".upload-box");
+    const label = box.find(".upload-content span");
+    const file = this.files[0];
+
+    if (file) {
+      label.text(file.name);
+    } else {
+      label.text(label.data("plceholder"));
+    }
+  });
+});
